54d03d3ca2209997356ef26dc44d4ce90f0a61f6
iglooe <day.derek7@protonmail.com>
2023-10-14 04:17:02 -0500

    landing page done

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-4xl md:text-5xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Notary</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Notary is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+    </div>
+  );
+};
